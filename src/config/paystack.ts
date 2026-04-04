2e41ac2e34d2ca3c8e35ccbaed37d84948fd8829
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2026-04-04 22:59:51 +0100

    hopefully resolved build error

--- src/config/paystack.ts
+++ src/config/paystack.ts
@@ -1,6 +1,6 @@
 import config from "@/config/config.js";
 import Paystack from "@paystack/paystack-sdk";
 // const paystack = new Paystack(config.paystack.secretKey);
-const paystack = {};
+const paystack = {} as any;
 
 export default paystack;
