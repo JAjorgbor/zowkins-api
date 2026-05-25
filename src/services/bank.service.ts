b81656eb61a77780eb1c461573919cbb33f6b8a7
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2026-05-25 05:59:47 +0100

    resolved build error

--- src/services/bank.service.ts
+++ src/services/bank.service.ts
@@ -1,19 +1,19 @@
 import paystack from "@/config/paystack.js";
 
 const getBanks = async () => {
-  const { data } = await paystack.verification.fetchBanks({
-    country: "Nigeria",
-  });
-  return data;
+  // const { data } = await paystack.verification.fetchBanks({
+  //   country: "Nigeria",
+  // });
+  return undefined;
 };
 
 const validateBankAccount = async (accountNumber: string, bankCode: string) => {
-  const response = await paystack.verification.resolveAccountNumber({
-    account_number: accountNumber,
-    //   bank_code: bankCode,
-    bank_code: "001",
-  });
-  return response;
+  // const response = await paystack.verification.resolveAccountNumber({
+  //   account_number: accountNumber,
+  //   //   bank_code: bankCode,
+  //   bank_code: "001",
+  // });
+  return undefined;
 };
 
 export default { getBanks, validateBankAccount };
